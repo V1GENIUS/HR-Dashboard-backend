f4a3f9a5aa8fcbbce6d440b1b93f9876b1d0e9d6
Vivek rathore <93070457+V1GENIUS@users.noreply.github.com>
2025-05-02 06:29:45 +0000

    port changes

--- Database/db.js
+++ Database/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const db = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://SarkariGenius:Ektuhinirankar123@geniuscluster.wsjvs.mongodb.net/?retryWrites=true&w=majority');
+    const conn = await mongoose.connect('mongodb+srv://vivekpractice:sVMap0oJTRuVkb64@practicecluster0.n9ceii3.mongodb.net/?retryWrites=true&w=majority&appName=practiceCluster0');
     console.log("Successfully connected to MongoDB!");
   } catch (error) {
     console.error("Failed to connect to MongoDB:", error);
